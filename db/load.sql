da53ff6de658fe997349b198e5c9578f8a8c7490
VoidCorner <140381142+VoidCorner@users.noreply.github.com>
2023-10-05 20:48:58 -0400

    Update load.sql

--- db/load.sql
+++ db/load.sql
@@ -14,3 +14,13 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
+
+\COPY Carts FROM 'Carts.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.carts_id_seq',
+                         (SELECT MAX(id)+1 FROM Carts),
+                         false);
+
+\COPY Review FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.review_review_id_seq',
+                         (SELECT MAX(review_id)+1 FROM Review),
+                         false);
